37e29f13e6a7ed64203925e7b339dc16a4a6e158
Yunus Mustapha <yunusmustapha2.com>
2023-11-30 14:44:12 +0100

    Open health server

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const { OpenAI } = require("openai"); // Correct import statement
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+app.post("/symptoms/askai", async (req, res) => {
+  const { symptoms, ageRange, gender, fullName } = req.body;
+
+   console.log("Received data:", { symptoms, ageRange, gender, fullName });
+
+  // Construct the prompts for OpenAI
+  const prompt1 = `I would appreciate your help in providing some suggestions or insights. My symptoms include:\n symptoms: ${symptoms}\n age range: ${ageRange}\n gender: ${gender}. Summarize the response to 100 words`;
+
+  const prompt2 = `What could be the possible causes of these symptoms?\n symptoms: ${symptoms}\n age range: ${ageRange}\n gender: ${gender}. Summarize the response to 100 words`;
+
+  const prompt3 = `What to do next if I notice these symptoms?\n symptoms: ${symptoms}\n age range: ${ageRange}\n gender: ${gender}. Summarize the response to 100 words`;
+
+  try {
+    const symptomsCheck = await ChatGPTFunction(prompt1);
+    const causes = await ChatGPTFunction(prompt2);
+    const treatment = await ChatGPTFunction(prompt3);
+
+    const responseData = {
+      symptomsCheck,
+      causes,
+      treatment,
+    };
+
+    res.status(200).json({
+      message: "Request successful!",
+      data: responseData,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+const ChatGPTFunction = async (message) => {
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: message }],
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+  
+    return response.choices[0].message.content;
+  } catch (error) {
+    console.error("ChatGPTFunction Error:", error);
+    throw error; // Re-throw the error to be caught by the calling function
+  }
+};
